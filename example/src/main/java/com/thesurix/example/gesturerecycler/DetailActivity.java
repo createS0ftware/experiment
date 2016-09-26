5a406e6b72f2a2ed7b5ec7b8f628f54f35768083
Hanson Aboagye <hanson@higher-thoughts.co.uk>
2016-09-26 08:14:03 +0100

    animation update

--- example/src/main/java/com/thesurix/example/gesturerecycler/DetailActivity.java
+++ example/src/main/java/com/thesurix/example/gesturerecycler/DetailActivity.java
@@ -6,12 +6,15 @@ import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v4.animation.ValueAnimatorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
@@ -27,6 +30,8 @@ public class DetailActivity extends AppCompatActivity {
 
     @Bind(R.id.month_image2)
     ImageView detailImage;
+    @Bind(R.id.view)
+    CardView cardView;
 
     @Bind(R.id.mainLayout)
     RelativeLayout relativeLayout;
@@ -48,15 +53,15 @@ public class DetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         monthText.setText("MAY");
         detailImage.setImageDrawable(getResources().getDrawable(getIntent().getIntExtra("image",0)));
+
         startAnimation();
     }
 
     private void startAnimation()
     {
-        final TranslateAnimation slideUpDrawer = new TranslateAnimation(0,0, scrollView.getHeight(), -1600);
-        slideUpDrawer.setDuration(800);
-        slideUpDrawer.setFillAfter(true);
-        slideUpDrawer.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        final Animation bottomUp = AnimationUtils.loadAnimation(this,
+                R.anim.slide_up);
 
         AlphaAnimation fadeInAnimation = new AlphaAnimation(0f, 1f);
         fadeInAnimation.setDuration(400L);
@@ -70,7 +75,9 @@ public class DetailActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                scrollView.startAnimation(slideUpDrawer);
+                scrollView.startAnimation(bottomUp);
+                scrollView.setVisibility(View.VISIBLE);
+
             }
 
             @Override
--- example/src/main/java/com/thesurix/example/gesturerecycler/fragment/LinearRecyclerFragment.java
+++ example/src/main/java/com/thesurix/example/gesturerecycler/fragment/LinearRecyclerFragment.java
@@ -35,8 +35,6 @@ public class LinearRecyclerFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_recycler, container, false);
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
-
-
         return rootView;
     }
 
